b6a950cf43f36c714a703739facba31d92e058af
Sz-Melinda <szikora.mleinda.24@gmail.com>
2022-02-15 15:52:00 +0100

    "Kis galéria reszponzivitása"

---
+++ js/foglalasFelugro.js
@@ -0,0 +1,14 @@
+
+var modal = document.getElementById("foglFelugro");
+            
+var btn = document.getElementById("foglalasGomb");
+            
+btn.onclick = function() {
+
+    modal.style.display = "block";
+}
+            
+span.onclick = function() {
+
+    modal.style.display = "none";
+}
