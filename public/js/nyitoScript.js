ee4cb4cd61558a68f24501517b1b350311ef1072
szmelinda <szikora.melinda.24@gmail.com>
2022-05-03 13:21:58 +0200

    pull

--- public/js/nyitoScript.js
+++ public/js/nyitoScript.js
@@ -65,7 +65,7 @@ $(function() {
         let sablonElem = $("#korhintaGaleria");
 
         const ujElem = sablonElem.clone().appendTo(galeriaSzulo);
-        ujGaleriaPlakat = new GaleriaPlakat(ujElem, plakatTomb[1]);
+        ujGaleriaPlakat = new GaleriaPlakat(ujElem, plakatTomb[0]);
 
         sablonElem.remove();
     }
